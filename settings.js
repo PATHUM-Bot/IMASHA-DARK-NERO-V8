d0cde05f97562dd75d9cfb22c4ad101be67b68d0
PATHUM_BOT <101706769+PATHUM-Bot@users.noreply.github.com>
2023-04-14 21:16:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,25 +20,25 @@ global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'off' //inbox block
 global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
 global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0786802371'] //ur owner number
-global.ownername = "kaveesha" //ur owner name
-global.ytname = "YT: Dark" //ur yt chanel name
+global.owner = ['0778639556'] //ur owner number
+global.ownername = "Imasha WhatsApp Bot" //ur owner name
+global.ytname = "YT: IP MODZ" //ur yt chanel name
 global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = " " //session Id
-global.ownernomer = "94786802371" //ur number
+global.session = "NzZVcDdoVXc=" //session Id
+global.ownernomer = "94778639556" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
-global.premium = ['94786802371'] //ur premium number
+global.premium = ['94778639556'] //ur premium number
 global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://www.darknero.ga/' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Dark Nero By" //ur sticker watermark packname
-global.author = "Dark Nero Creating By Cyber Yakuza Team" //ur sticker watermark author
-global.wm = "Dark Bot Inc." //ur watermark
+global.author = "Dark Nero Creating By Imasha WhatsApp Bot" //ur sticker watermark author
+global.wm = "Imasha Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
